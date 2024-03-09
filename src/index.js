e1be68f428ed1318f7e11486d157ed9831973fee
gaurav <gaurav30012000jha@gmail.com>
2024-03-09 20:20:32 +0530

    nodemailer for notifications smpt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {PORT}=require('./config/serverConfig')
+const {sendBasicEmail}=require('./services/email-service')
+
 const app = express();
 const setupAndStartServer = () => {
     const app = express();
@@ -9,6 +11,13 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
+        sendBasicEmail(
+            'ManishMehta@gmail.com',
+            'satyam987321@gmail.com',
+            'Escalations@Cvent.in',
+            'IMPORTANT, hey satyam please see this immediately'
+        
+        )
     });
 
 
