576d1f2216e6f8404d3f2e46426cf207086485d8
gaurav <gaurav30012000jha@gmail.com>
2024-03-17 13:29:00 +0530

    "added consumer logic and setup email reminder service messaging queues"

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {PORT}=require('./config/serverConfig')
-const {sendBasicEmail}=require('./services/email-service')
+const {PORT, REMINDER_BINDING_KEY}=require('./config/serverConfig')
+//const {sendBasicEmail}=require('./services/email-service')
+const {createChannel, susbscribeMessage}=require('./utils/messageQueue')
 const jobs=require('../src/utils/cron-job');
+const TicketController=require('./controllers/ticket-controller');
+const EmailService=require('./services/email-service');
 const app = express();
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
-    app.listen(PORT, () => {
+    app.post('api/v1/tickets',TicketController.create);
+    const channel= await createChannel();
+    susbscribeMessage(channel,EmailService.subscribeEvents,REMINDER_BINDING_KEY);
+    
+    app.listen(PORT,  () => {
         console.log(`Server started at port ${PORT}`);
-        // sendBasicEmail(
-        //     'ManishMehta@gmail.com',
-        //     'satyam987321@gmail.com',
-        //     'Escalations@Cvent.in',
-        //     'IMPORTANT, hey satyam please see this immediately'
-        
-        // )
-        jobs();
+        //jobs();
     });
 
 
